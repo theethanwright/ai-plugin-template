c3542d33b86e670639cf73ee963f96a479654ac0
theethanwright <theethanwright@gmail.com>
2024-07-02 13:10:54 -0400

    Starting to change things

--- .history/app/page_20240702122026.tsx
+++ .history/app/page_20240702122026.tsx
@@ -136,79 +136,6 @@ export default function Plugin() {
     }
   };
 
-  const onScanBrand = async () => {
-    const layers = await getTextForSelection();
-
-    if (!layers.length) {
-      figmaAPI.run(async (figma) => {
-        figma.notify(
-          "Please select a layer with text in it to generate a poem.",
-          { error: true },
-        );
-      });
-      return;
-    }
-
-    const reader = await streamAIResponse({
-      layers,
-    });
-
-    let text = "";
-    let nodeID: string | null = null;
-    const textPosition = await getTextOffset();
-
-    const createOrUpdateTextNode = async () => {
-      // figmaAPI.run is a helper that lets us run code in the figma plugin sandbox directly
-      // from the iframe without having to post messages back and forth. For more info,
-      // see /lib/figmaAPI.ts
-      //
-      // It is important to note that any variables that this function closes over must be
-      // specified in the second argument to figmaAPI.run. This is because the code is actually
-      // run in the figma plugin sandbox, not in the iframe.
-      nodeID = await figmaAPI.run(
-        async (figma, { nodeID, text, textPosition }) => {
-          let node = figma.getNodeById(nodeID ?? "");
-
-          // If the node doesn't exist, create it and position it to the right of the selection.
-          if (!node) {
-            node = figma.createText();
-            node.x = textPosition?.x ?? 0;
-            node.y = textPosition?.y ?? 0;
-          }
-
-          if (node.type !== "TEXT") {
-            return "";
-          }
-
-          const oldHeight = node.height;
-
-          await figma.loadFontAsync({ family: "Inter", style: "Medium" });
-          node.fontName = { family: "Inter", style: "Medium" };
-
-          node.characters = text;
-
-          // Scroll and zoom to the node if it's height changed (ex we've added a new line).
-          // We only do this when the height changes to reduce flickering.
-          if (oldHeight !== node.height) {
-            figma.viewport.scrollAndZoomIntoView([node]);
-          }
-
-          return node.id;
-        },
-        { nodeID, text, textPosition },
-      );
-    };
-
-    while (true) {
-      const { done, value } = await reader.read();
-      if (done) {
-        break;
-      }
-      text += value;
-      await createOrUpdateTextNode();
-    }
-  };
-
   return (
     <div className="flex flex-col items-center min-h-screen bg-gray-900 text-white">
       <h1 className="text-4xl font-bold mb-5 mt-2">Poem Generator</h1>
@@ -227,12 +154,6 @@ export default function Plugin() {
           className="mb-5 p-2 px-4 rounded bg-green-600 text-white hover:bg-green-700"
         >
           Generate Poem on Canvas
-        </button>
-        <button
-          onClick={onScanBrand}
-          className="mb-5 p-2 px-4 rounded bg-green-600 text-white hover:bg-green-700"
-        >
-          Scan Brand
         </button>
       </div>
       {completion && (
